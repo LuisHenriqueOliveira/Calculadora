f154ee7d4fe2c2337d2247fa6054a1a95a842a87
arqdsis <arqdsis@MOOLI10130.animaedu.intranet>
2020-02-21 18:53:15 -0300

    Segunda Versao - subtracao adicionada

--- Calculadora.java
+++ Calculadora.java
@@ -2,17 +2,19 @@ import java.util.Scanner;
 public class Calculadora{
 	public static void main(String[] args){
 		Scanner entrada = new Scanner(System.in);
-		float Entrada1 = 0;
-		float Entrada2 = 0;
-		float soma = 0;
+		int Entrada1 = 0;
+		int Entrada2 = 0;
+		int soma = 0;
+      int subtracao = 0;
 		
 		System.out.print("Digite o primeiro numero: ");
-		Entrada1 = entrada.nextFloat();
+		Entrada1 = entrada.nextInt();
 		System.out.print("Digite o segundo numero: ");
-		Entrada2 = entrada.nextFloat();
+		Entrada2 = entrada.nextInt();
 		
 		soma = Entrada1+Entrada2;
-		
-		System.out.printf("%.2f\n",soma);
+		subtracao = Entrada1+Entrada2;
+		System.out.printf("O Resultado da soma: %d\n",soma);
+      System.out.printf("O Resultado da subtracao: %d\n",subtracao);
 	}
 }
