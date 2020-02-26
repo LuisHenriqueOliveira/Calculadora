3dbe1241bc12a8e64e2a7a6229b009ee768e5ab6
Luis Henrique <rickoliveira10@hotmail.com>
2020-02-26 19:33:37 -0300

    Primeira Versao - Soma

--- Calculadora.java
+++ Calculadora.java
@@ -2,19 +2,17 @@ import java.util.Scanner;
 public class Calculadora{
 	public static void main(String[] args){
 		Scanner entrada = new Scanner(System.in);
-		int Entrada1 = 0;
-		int Entrada2 = 0;
+		int NSoma1 = 0;
+		int NSoma2 = 0;
 		int soma = 0;
-      int subtracao = 0;
 		
-		System.out.print("Digite o primeiro numero: ");
-		Entrada1 = entrada.nextInt();
-		System.out.print("Digite o segundo numero: ");
-		Entrada2 = entrada.nextInt();
+		System.out.print("Digite o primeiro numero para a soma: ");
+		NSoma1 = entrada.nextInt();
+		System.out.print("Digite o segundo numero para a soma: ");
+		NSoma2 = entrada.nextInt();
 		
 		soma = Entrada1+Entrada2;
-		subtracao = Entrada1+Entrada2;
+      
 		System.out.printf("O Resultado da soma: %d\n",soma);
-      System.out.printf("O Resultado da subtracao: %d\n",subtracao);
 	}
 }
